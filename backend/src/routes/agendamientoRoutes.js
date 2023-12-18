b92971e5b3411e86d4bca98a40fe750849f8336e
Matias <matias.lagos2101@alumnos.ubiobio.cl>
2023-12-18 19:06:06 -0300

    frontend

--- backend/src/routes/agendamientoRoutes.js
+++ backend/src/routes/agendamientoRoutes.js
@@ -4,12 +4,10 @@ const agendamientoController = require("../controllers/agendamientoController");
 const router = express.Router();
 
 // Rutas sin middleware de autenticación
-router.get("/obteneragendamiento", agendamientoController.getAllAgendamientos);
-router.get("/obtenerdataagendamiento:id", agendamientoController.getAgendamientoById);
-
-// Rutas con middleware de autenticación y autorización
 router.post("/agregaragendamiento", agendamientoController.createAgendamiento);
-router.put("/actualizaagendamiento:id", agendamientoController.updateAgendamiento);
-router.delete("/borraragendamiento:id", agendamientoController.deleteAgendamiento);
+router.get("/obteneragendamiento", agendamientoController.getAllAgendamientos);
+router.get("/obtenerdataagendamiento/:id", agendamientoController.getAgendamientoById);
+router.put("/actualizaagendamiento/:id", agendamientoController.updateAgendamiento); 
+router.delete("/borraragendamiento/:id", agendamientoController.deleteAgendamiento);
 
 module.exports = router;
